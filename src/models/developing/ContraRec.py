a2876b801a74812df3d17c31a9f76c503cb8702f
THUwangcy <THUwangcy@gmail.com>
2021-03-03 18:41:53 +0800

    Implement pure ContraRec

---
+++ src/models/developing/ContraRec.py
@@ -0,0 +1,246 @@
+# -*- coding: UTF-8 -*-
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+from models.BaseModel import SequentialModel
+from utils import layers
+
+
+class ContraRec(SequentialModel):
+    extra_log_args = ['gamma', 'batch_size', 'temp', 'encoder']
+
+    @staticmethod
+    def parse_model_args(parser):
+        parser.add_argument('--emb_size', type=int, default=64,
+                            help='Size of embedding vectors.')
+        parser.add_argument('--gamma', type=float, default=1,
+                            help='Coefficient of the contrastive loss.')
+        parser.add_argument('--beta_a', type=int, default=3,
+                            help='Parameter of the beta distribution for sampling.')
+        parser.add_argument('--beta_b', type=int, default=3,
+                            help='Parameter of the beta distribution for sampling.')
+        parser.add_argument('--temp', type=float, default=0.2,
+                            help='Temperature in contrastive loss.')
+        parser.add_argument('--encoder', type=str, default='BERT4Rec',
+                            help='Choose a sequence encoder: GRU4Rec, Caser, BERT4Rec.')
+        return SequentialModel.parse_model_args(parser)
+
+    def __init__(self, args, corpus):
+        self.emb_size = args.emb_size
+        self.max_his = args.history_max
+        self.gamma = args.gamma
+        self.beta_a = args.beta_a
+        self.beta_b = args.beta_b
+        self.temperature = args.temp
+        self.encoder_name = args.encoder
+        super().__init__(args, corpus)
+
+    def _define_params(self):
+        self.i_embeddings = nn.Embedding(self.item_num, self.emb_size)
+        if self.encoder_name == 'GRU4Rec':
+            self.encoder = GRU4RecEncoder(self.emb_size, hidden_size=128)
+        elif self.encoder_name == 'Caser':
+            self.encoder = CaserEncoder(self.emb_size, self.max_his, num_horizon=16, num_vertical=8, l=5)
+        elif self.encoder_name == 'BERT4Rec':
+            self.encoder = BERT4RecEncoder(self.emb_size, self.max_his, self.device, num_layers=2, num_heads=2)
+        else:
+            raise ValueError('Invalid sequence encoder.')
+        self.criterion = ContraLoss(self.device, temperature=self.temperature)
+
+    def forward(self, feed_dict):
+        self.check_list = []
+        i_ids = feed_dict['item_id']  # bsz, n_candidate
+        history = feed_dict['history_items']  # bsz, history_max
+        lengths = feed_dict['lengths']  # bsz
+
+        his_vectors = self.i_embeddings(history)
+        his_vector = self.encoder(his_vectors, lengths)
+        i_vectors = self.i_embeddings(i_ids)
+        prediction = (his_vector[:, None, :] * i_vectors).sum(-1)
+        out_dict = {'prediction': prediction}
+
+        if feed_dict['phase'] == 'train':
+            history_aug = feed_dict['history_items_aug']
+            his_aug_vectors = self.i_embeddings(history_aug)
+            his_aug_vector = self.encoder(his_aug_vectors, lengths)
+            features = torch.stack([his_vector, his_aug_vector], dim=1)  # bsz, 2, emb
+            features = F.normalize(features, dim=-1)
+            out_dict['features'] = features  # bsz, 2, emb
+            out_dict['labels'] = i_ids[:, 0]  # bsz
+
+        return out_dict
+
+    def loss(self, out_dict):
+        contra_loss = self.criterion(out_dict['features'], labels=out_dict['labels'])
+        loss = super().loss(out_dict) + self.gamma * contra_loss
+        return loss
+
+    class Dataset(SequentialModel.Dataset):
+        def reorder_op(self, seq):
+            ratio = np.random.beta(a=self.model.beta_a, b=self.model.beta_b)
+            select_len = int(len(seq) * ratio)
+            start = np.random.randint(0, len(seq) - select_len + 1)
+            idx_range = np.arange(len(seq))
+            np.random.shuffle(idx_range[start: start + select_len])
+            return np.array(seq)[idx_range]
+
+        def _get_feed_dict(self, index):
+            feed_dict = super()._get_feed_dict(index)
+            if self.phase == 'train':
+                history_items = self.reorder_op(feed_dict['history_items'])
+                history_items_aug = self.reorder_op(feed_dict['history_items'])
+                feed_dict['history_items'] = history_items
+                feed_dict['history_items_aug'] = history_items_aug
+            return feed_dict
+
+
+""" Contrastive Loss """
+class ContraLoss(nn.Module):
+    def __init__(self, device, temperature=0.2):
+        super(ContraLoss, self).__init__()
+        self.device = device
+        self.temperature = temperature
+
+    def forward(self, features, labels=None, mask=None):
+        """
+        If both `labels` and `mask` are None, it degenerates to SimCLR loss
+        Args:
+            features: hidden vector of shape [bsz, n_views, ...].
+            labels: target item of shape [bsz].
+            mask: contrastive mask of shape [bsz, bsz], mask_{i,j}=1 if sequence j
+                has the same target item as sequence i. Can be asymmetric.
+        Returns:
+            A loss scalar.
+        """
+        if len(features.shape) < 3:
+            raise ValueError('`features` needs to be [bsz, n_views, ...],'
+                             'at least 3 dimensions are required')
+        if len(features.shape) > 3:
+            features = features.view(features.shape[0], features.shape[1], -1)
+
+        batch_size = features.shape[0]
+        if labels is not None and mask is not None:
+            raise ValueError('Cannot define both `labels` and `mask`')
+        elif labels is None and mask is None:
+            mask = torch.eye(batch_size, dtype=torch.float32).to(self.device)
+        elif labels is not None:
+            labels = labels.contiguous().view(-1, 1)
+            if labels.shape[0] != batch_size:
+                raise ValueError('Num of labels does not match num of features')
+            mask = torch.eq(labels, labels.transpose(0, 1)).float().to(self.device)
+
+        contrast_count = features.shape[1]  # n_views
+        contrast_feature = torch.cat(torch.unbind(features, dim=1), dim=0)  # bsz * n_views, -1
+
+        # compute logits
+        anchor_dot_contrast = torch.matmul(contrast_feature, contrast_feature.transpose(0, 1)) /self.temperature
+        # for numerical stability
+        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
+        logits = anchor_dot_contrast - logits_max.detach()  # bsz * n_views, bsz * n_views
+
+        # tile mask
+        mask = mask.repeat(contrast_count, contrast_count)  # bsz * n_views, bsz * n_views
+        # mask-out self-contrast cases
+        logits_mask = torch.scatter(
+            torch.ones_like(mask), 1,
+            torch.arange(mask.shape[0]).view(-1, 1).to(self.device), 0
+        )
+        mask = mask * logits_mask
+
+        # compute log_prob
+        exp_logits = torch.exp(logits) * logits_mask
+        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True) + 1e-10)
+
+        # compute mean of log-likelihood over positive
+        mean_log_prob_pos = (mask * log_prob).sum(1) / (mask.sum(1) + 1e-10)
+
+        # loss
+        loss = - self.temperature * mean_log_prob_pos
+        return loss.mean()
+
+""" Encoder Layers """
+class GRU4RecEncoder(nn.Module):
+    def __init__(self, emb_size, hidden_size=128):
+        super().__init__()
+        self.rnn = nn.GRU(input_size=emb_size, hidden_size=hidden_size, batch_first=True)
+        self.out = nn.Linear(hidden_size, emb_size, bias=False)
+
+    def forward(self, seq, lengths):
+        # Sort and Pack
+        sort_lengths, sort_idx = torch.topk(lengths, k=len(lengths))
+        sort_seq = seq.index_select(dim=0, index=sort_idx)
+        seq_packed = torch.nn.utils.rnn.pack_padded_sequence(sort_seq, sort_lengths, batch_first=True)
+
+        # RNN
+        output, hidden = self.rnn(seq_packed, None)
+
+        # Unsort
+        sort_rnn_vector = self.out(hidden[-1])
+        unsort_idx = torch.topk(sort_idx, k=len(lengths), largest=False)[1]
+        rnn_vector = sort_rnn_vector.index_select(dim=0, index=unsort_idx)
+
+        return rnn_vector
+
+class CaserEncoder(nn.Module):
+    def __init__(self, emb_size, max_his, num_horizon=16, num_vertical=8, l=5):
+        super().__init__()
+        self.max_his = max_his
+        lengths = [i + 1 for i in range(l)]
+        self.conv_h = nn.ModuleList(
+            [nn.Conv2d(in_channels=1, out_channels=num_horizon, kernel_size=(i, emb_size)) for i in lengths])
+        self.conv_v = nn.Conv2d(in_channels=1, out_channels=num_vertical, kernel_size=(max_his, 1))
+        self.fc_dim_h = num_horizon * len(lengths)
+        self.fc_dim_v = num_vertical * emb_size
+        fc_dim_in = self.fc_dim_v + self.fc_dim_h
+        self.fc = nn.Linear(fc_dim_in, emb_size)
+
+    def forward(self, seq, lengths):
+        batch_size, seq_len = seq.size(0), seq.size(1)
+        pad_len = self.max_his - seq_len
+        seq = F.pad(seq, [0, 0, 0, pad_len]).unsqueeze(1)
+
+        # Convolution Layers
+        out_v = self.conv_v(seq).view(-1, self.fc_dim_v)
+        out_hs = list()
+        for conv in self.conv_h:
+            conv_out = conv(seq).squeeze(3).relu()
+            pool_out = F.max_pool1d(conv_out, conv_out.size(2)).squeeze(2)
+            out_hs.append(pool_out)
+        out_h = torch.cat(out_hs, 1)
+
+        # Fully-connected Layers
+        his_vector = self.fc(torch.cat([out_v, out_h], 1))
+        return his_vector
+
+class BERT4RecEncoder(nn.Module):
+    def __init__(self, emb_size, max_his, device, num_layers=2, num_heads=2):
+        super().__init__()
+        self.device = device
+
+        self.p_embeddings = nn.Embedding(max_his + 1, emb_size)
+        self.transformer_block = nn.ModuleList([
+            layers.TransformerLayer(d_model=emb_size, d_ff=emb_size, n_heads=num_heads)
+            for _ in range(num_layers)
+        ])
+
+    def forward(self, seq, lengths):
+        batch_size, seq_len = seq.size(0), seq.size(1)
+        len_range = torch.from_numpy(np.arange(seq_len)).to(self.device)
+        valid_mask = len_range[None, :] < lengths[:, None]
+
+        # Position embedding
+        position = len_range[None, :] * valid_mask.long()
+        pos_vectors = self.p_embeddings(position)
+        seq = seq + pos_vectors
+
+        # Self-attention
+        attn_mask = valid_mask.view(batch_size, 1, 1, seq_len)
+        for block in self.transformer_block:
+            seq = block(seq, attn_mask)
+        seq = seq * valid_mask[:, :, None].float()
+
+        his_vector = seq[torch.arange(batch_size), lengths - 1]
+        return his_vector
--- src/run.sh
+++ src/run.sh
@@ -15,15 +15,15 @@ python main.py --model_name Caser --emb_size 64 --L 5 --num_horizon 64 --num_ver
 
 python main.py --model_name SASRec --emb_size 64 --num_layers 1 --num_heads 1 --lr 1e-4 --l2 1e-6 --history_max 20 --dataset 'Grocery_and_Gourmet_Food'
 
+python main.py --model_name ContraRec --emb_size 64 --lr 1e-4 --l2 1e-6 --history_max 20 --encoder BERT4Rec --gamma 1 --temp 0.2 --batch_size 4096 --dataset 'Grocery_and_Gourmet_Food'
+
 python main.py --model_name TiSASRec --emb_size 64 --num_layers 1 --num_heads 1 --lr 1e-4 --l2 1e-6 --history_max 20 --dataset 'Grocery_and_Gourmet_Food'
 
-python main.py --model_name CFKG --emb_size 64 --margin 1 --include_attr 0 --lr 1e-4 --l2 1e-8 --dataset 'Grocery_and_Gourmet_Food'
+python main.py --model_name CFKG --emb_size 64 --margin 1 --include_attr 1 --lr 1e-4 --l2 1e-8 --dataset 'Grocery_and_Gourmet_Food'
 
 python main.py --model_name SLRCPlus --emb_size 64 --lr 5e-4 --l2 1e-5 --dataset 'Grocery_and_Gourmet_Food'
 
-python main.py --model_name Chorus --emb_size 64 --margin 1 --lr 5e-4 --l2 1e-5 --check_epoch 10 --epoch 50 --early_stop 0 --batch_size 512 --dataset 'Grocery_and_Gourmet_Food' --stage 1
-python main.py --model_name Chorus --emb_size 64 --margin 1 --lr_scale 0.1 --lr 1e-3 --l2 0 --check_epoch 10 --dataset 'Grocery_and_Gourmet_Food' --base_method 'BPR' --stage 2
+python main.py --model_name Chorus --emb_size 64 --margin 1 --lr 5e-4 --l2 1e-5 --epoch 50 --early_stop 0 --batch_size 512 --dataset 'Grocery_and_Gourmet_Food' --stage 1
+python main.py --model_name Chorus --emb_size 64 --margin 1 --lr_scale 0.1 --lr 1e-3 --l2 0 --dataset 'Grocery_and_Gourmet_Food' --base_method 'BPR' --stage 2
 
 python main.py --model_name KDA --emb_size 64 --include_attr 1 --freq_rand 0 --lr 1e-3 --l2 1e-6 --num_heads 4 --history_max 20 --dataset 'Grocery_and_Gourmet_Food'
-
-python main.py --model_name SRGNN --emb_size 64 --num_layers 1 --lr 1e-4 --l2 1e-6 --history_max 20 --dataset 'Grocery_and_Gourmet_Food'
