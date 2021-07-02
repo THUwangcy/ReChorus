ca07914a674ef713dca7551067580f3e04fd4db2
THUwangcy <THUwangcy@gmail.com>
2021-07-02 17:32:23 +0800

    Implement BUIR

---
+++ src/helpers/MoRunner.py
@@ -0,0 +1,39 @@
+# -*- coding: UTF-8 -*-
+
+import os
+import gc
+import torch
+import torch.nn as nn
+import logging
+import numpy as np
+from time import time
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from typing import Dict, List, NoReturn
+
+from utils import utils
+from models.BaseModel import BaseModel
+from helpers.BaseRunner import BaseRunner
+
+
+class MomenRunner(BaseRunner):
+    def fit(self, data: BaseModel.Dataset, epoch=-1) -> float:
+        model = data.model
+        if model.optimizer is None:
+            model.optimizer = self._build_optimizer(model)
+        data.actions_before_epoch()  # must sample before multi thread start
+
+        model.train()
+        loss_lst = list()
+        dl = DataLoader(data, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers,
+                        collate_fn=data.collate_batch, pin_memory=self.pin_memory)
+        for batch in tqdm(dl, leave=False, desc='Epoch {:<3}'.format(epoch), ncols=100, mininterval=1):
+            batch = utils.batch_to_gpu(batch, model.device)
+            model.optimizer.zero_grad()
+            out_dict = model(batch)
+            loss = model.loss(out_dict)
+            loss.backward()
+            model.optimizer.step()
+            model._update_target()
+            loss_lst.append(loss.detach().cpu().data.numpy())
+        return np.mean(loss_lst).item()
